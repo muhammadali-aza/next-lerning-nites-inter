2adc92d66da2ac6972f5edee96e10bedfa22f75b
muhammadali-aza <alirazaitservice@gmail.com>
2026-03-25 13:11:05 +0500

    nested routing

--- routing/app/services/page.js
+++ routing/app/services/page.js
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export default function Services() {
     return (
         <>
             <h1>Services</h1>
+            <p><Link href="/services/webdev">Web Developement</Link></p>
+            <p><Link href="/">App Developement</Link></p>
+            <p><Link href="/">Web Design</Link></p>
+            <p><Link href="/">App Design</Link></p>
         </>
     )
 }
---
+++ routing/app/services/webdev/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div>
+            <h1>Website Development</h1>
+        </div>
+    )
+}
